4702942a8899422fd134c07858ddf0e78e8db5e2
Galiwango Ananiya <galiwango101@gmail.com>
2021-10-19 21:22:07 +0300

    Refactor Pet function into Pet component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Pet from "./Pet";
 
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
