c5593d10301eb507f75d31291af5ab002102e95a
Galiwango Ananiya <galiwango101@gmail.com>
2021-10-19 19:50:30 +0300

    Add and configure prettier.

--- src/App.js
+++ src/App.js
@@ -3,23 +3,28 @@ const Pet = (props) => {
     React.createElement("h2", {}, props.name),
     React.createElement("h3", {}, props.animal),
     React.createElement("h3", {}, props.breed),
-  ])
-}
-
-const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    [
-      React.createElement("h1", {id: "my-brand"}, "Adopt Me!"),
-      React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havanese"}),
-      React.createElement(Pet, { name: "Pepper", animal: "Bird", breed: "Cockatiel"}),
-      React.createElement(Pet, { name: "Sudo", animal: "Dog", breed: "Wheaten Terrier"}),
-    ]
-  );
+  ]);
 };
 
-ReactDOM.render(
-  React.createElement(App),
-  document.getElementById("root")
-);
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, {
+      name: "Sudo",
+      animal: "Dog",
+      breed: "Wheaten Terrier",
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
