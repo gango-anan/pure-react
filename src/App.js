16728d644be7e9119be6c5708abee7480627ad44
Galiwango Ananiya <galiwango101@gmail.com>
2021-10-19 03:12:41 +0300

    Add props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-const Pet = () => {
+const Pet = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h2", {}, "Luna"),
-    React.createElement("h3", {}, "Dog"),
-    React.createElement("h3", {}, "Havanese"),
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
   ])
 }
 
@@ -11,10 +11,10 @@ const App = () => {
     "div",
     {},
     [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet),
-      React.createElement(Pet),
-      React.createElement(Pet),
+      React.createElement("h1", {id: "my-brand"}, "Adopt Me!"),
+      React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havanese"}),
+      React.createElement(Pet, { name: "Pepper", animal: "Bird", breed: "Cockatiel"}),
+      React.createElement(Pet, { name: "Sudo", animal: "Dog", breed: "Wheaten Terrier"}),
     ]
   );
 };
