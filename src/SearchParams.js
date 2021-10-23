8a7eb5faf7141c2d04bd6d5454d814dc7088f582
Galiwango Ananiya <galiwango101@gmail.com>
2021-10-23 15:15:32 +0300

    Add hook to keep track of the breed that is selected.

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seatle, WA");
@@ -6,6 +7,9 @@ const SearchParams = () => {
   // const locationTuple = useState("Seatle, WA");
   // const location = locationTuple[0];
   // const setLocation = locationTuple[1];
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
 
   const updateLocation = (event) => {
     setLocation(event.target.value);
@@ -24,6 +28,50 @@ const SearchParams = () => {
             value={location}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(event) => setAnimal(event.target.value)}
+            onBlur={(event) => setAnimal(event.target.value)}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="location">
+          Location
+          <input
+            type="text"
+            id="location"
+            onChange={updateLocation}
+            // onChange={(e) => setLocation(e.target.value)}
+            value={location}
+            placeholder="Location"
+          />
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(event) => setBreed(event.target.value)}
+            onBlur={(event) => setBreed(event.target.value)}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option value={breed} key={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
