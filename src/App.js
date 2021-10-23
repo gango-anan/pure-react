2818e79343e1f67bb804b6f84149fe1de3c485a5
Galiwango Ananiya <galiwango101@gmail.com>
2021-10-23 14:19:50 +0300

    Add useState hook to manage location.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      {/* <Pet name="Luna" animal="Dog" breed="Havanese" />
       <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier" />
+      <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier" /> */}
+      <SearchParams />
     </div>
   );
 };
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,4 +1,16 @@
+import { useState } from "react";
+
 const SearchParams = () => {
+  const [location, setLocation] = useState("Seatle, WA");
+  // Another way of writing the above statement is
+  // const locationTuple = useState("Seatle, WA");
+  // const location = locationTuple[0];
+  // const setLocation = locationTuple[1];
+
+  const updateLocation = (event) => {
+    setLocation(event.target.value);
+  };
+
   return (
     <div className="search-params">
       <form>
@@ -7,7 +19,9 @@ const SearchParams = () => {
           <input
             type="text"
             id="location"
-            value="Seatle, WA"
+            onChange={updateLocation}
+            // onChange={(e) => setLocation(e.target.value)}
+            value={location}
             placeholder="Location"
           />
         </label>
